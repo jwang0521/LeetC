e1818b0c70b0d6e1aaccad0e2328205af4bf8659
Jessica Wang <145153632+jwang0521@users.noreply.github.com>
2025-09-23 11:43:54 -0500

    Time: 8 ms (50.06%) | Memory: 13.4 MB (54.97%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = sum(nums)
+        
+        for i in range(len(nums)):
+            right -= nums[i]
+            if(right == left):
+                return i
+            left += nums[i]
+        return -1
+        
